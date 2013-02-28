d10561865acbc048dcf5a2f96d10b403c6426806
Patrick <me@mail.com>
2013-02-28 13:39:54 +0100

    endret noe drit

--- src/test/TestClass.java
+++ src/test/TestClass.java
@@ -3,7 +3,8 @@ package test;
 public class TestClass {
 public static void main(String[] args) {
 // la til comment
-	System.out.println("WASSAAAAPPPPP");
-	System.out.println("asdasdasd");
+	System.out.println("WASSAAAPP");
+	System.out.println("asdasad");
+	System.out.println("aslkjdalksjdalskj");
 }
 }
