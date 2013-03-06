f62d939d98f405e1c54639bf31e7f06db528b00b
anon <no@ty.com>
2013-03-07 00:59:53 +0100

    moved view into gui

---
+++ src/gui/view/CalendarWindow.java
@@ -0,0 +1,18 @@
+package gui.view;
+
+import model.CalendarModel;
+import model.ChronosModel;
+
+public class CalendarWindow implements ChronosWindow {
+
+	private CalendarModel model;
+
+	public CalendarWindow(ChronosModel model) {
+		setModel(model);
+	}
+
+	@Override
+	public void setModel(ChronosModel model) {
+		this.model = (CalendarModel) model;
+	}
+}
---
+++ src/gui/view/ChronosWindow.java
@@ -0,0 +1,9 @@
+package gui.view;
+
+import model.ChronosModel;
+
+public interface ChronosWindow {
+
+	abstract public void setModel(ChronosModel model);
+
+}
