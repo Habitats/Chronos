f41617d2383f22b56884e723109a8aa7f08f8009
Jostein Solaas <josteinsolaas@gmail.com>
2013-03-11 14:00:31 +0100

    added CalViewHelper files

---
+++ src/client/gui/view/CalendarWindowHelper/CalLabel.java
@@ -0,0 +1,14 @@
+package client.gui.view.CalendarWindowHelper;
+
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+
+public class CalLabel extends JLabel{
+	
+	public CalLabel(String text) {
+		
+		super(text);
+		this.setHorizontalAlignment(SwingConstants.CENTER);
+	}
+
+}
