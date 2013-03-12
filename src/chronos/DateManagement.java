2888c150deeb6e471f4a3006d467d10aaa66c747
Jostein Solaas <josteinsolaas@gmail.com>
2013-03-12 10:57:03 +0100

    glemte save

--- src/chronos/DateManagement.java
+++ src/chronos/DateManagement.java
@@ -1,5 +1,12 @@
 package chronos;
 
+import java.util.Date;
+
 public class DateManagement {
+
+	public static String getFormatted(Date start) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
